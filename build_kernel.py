139a0e931c0663b347510b44e4a5129531ac749a
ryanking13 <def6488@gmail.com>
2019-03-15 00:20:43 +0900

    refactor build scripts

--- build_kernel.py
+++ build_kernel.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import argparse
 import getpass
 import re
 import subprocess as sp
@@ -12,64 +13,92 @@ def error(msg, **kwargs):
 def info(msg, **kwargs):
     print('\033[1;33m[*] {}\033[0m'.format(msg), **kwargs)
 
-if getpass.getuser() != 'root':
-    error('please run with sudo')
-    exit(1)
 
-_exec = sp.check_output
-if '-v' in sys.argv:
-    _exec = sp.call
+def find_device_name():
+    out = sp.check_output('dmesg')
+    msgs = out.split(b'\n')
+    for msg in msgs[::-1]:
+        msg = msg.decode()
+        if 'Attached SCSI removable disk' in msg:
+            device_name = re.findall('\[[a-zA-Z0-9]+\]', msg)
+            if not device_name:
+                error('USB not connected')
+                exit(1)
+            device_name = '/dev/{}'.format(device_name[0][1:-1])
+            info('USB is automatically recognized to {}'.format(device_name))
+            break
+    
+    return device_name
 
 
-# build all files
-info('Building...')
-build_cmd = './build-rpi3-arm64.sh'
-try:
-    _exec(build_cmd)
-except sp.CalledProcessError as e:
-    error('{} failed with return code {}'.format(build_cmd, e.returncode))
-    exit(1)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='print all messages while building kernel')
+    parser.add_argument('-e', '--eject', action='store_true',
+                        help='eject usb drive after build is complete')
+    parser.add_argument('--no-flash', action='store_true',
+                        help='do not flash kernel to USB, just build and exit')
+    parser.add_argument('--device', default=None,
+                        help='force device path (e.g. /dev/sdb), use this argument if device is not automatically detected')
+    
+    args = parser.parse_args()
+    return args
 
-# make boot img
-info('Generating boot image...')
-gen_boot_img_cmd = './scripts/mkbootimg_rpi3.sh'
-try:
-    _exec(gen_boot_img_cmd)
-except sp.CalledProcessError as e:
-    error('{} failed with return code {}'.format(gen_boot_img_cmd, e.returncode))
-    exit(1)
 
-info('Packing boot image...')
-pack_cmd = 'tar -zcvf tizen-unified_20181024.1_iot-boot-arm64-rpi3.tar.gz boot.img modules.img' 
-_exec(pack_cmd.split())
+def build():
+    if getpass.getuser() != 'root':
+        error('please run with sudo')
+        exit(1)
 
-# flash usb drive
-info ('Flashing use drive...')
-flash_cmd = './flash-sdcard.sh /dev/{}'
+    args = parse_args()
+    _exec = sp.call if args.verbose else sp.check_output
 
-out = sp.check_output('dmesg')
-msgs = out.split(b'\n')
 
-for msg in msgs[::-1]:
-    msg = msg.decode()
-    if 'Attached SCSI removable disk' in msg:
-        device_name = re.findall('\[[a-zA-Z0-9]+\]', msg)
-        if not device_name:
-            error('USB not connected')
-            exit(1)
-        device_name = device_name[0][1:-1]
-        info('USB is recognized to /dev/{}'.format(device_name))
-        flash_cmd = flash_cmd.format(device_name)
-        break
+    # build all files
+    info('Building...')
+    build_cmd = './build-rpi3-arm64.sh'
+    try:
+        _exec(build_cmd)
+    except sp.CalledProcessError as e:
+        error('{} failed with return code {}'.format(build_cmd, e.returncode))
+        exit(1)
 
-try:
-    p = sp.Popen('yes', stdout=sp.PIPE)
-    _exec(flash_cmd.split(), stdin=p.stdout)
-except sp.CalledProcessError as e:
-    error('{} failed with return code {}'.format(flash_cmd, e.returncode))
+    # make boot img
+    info('Generating boot image...')
+    gen_boot_img_cmd = './scripts/mkbootimg_rpi3.sh'
+    try:
+        _exec(gen_boot_img_cmd)
+    except sp.CalledProcessError as e:
+        error('{} failed with return code {}'.format(gen_boot_img_cmd, e.returncode))
+        exit(1)
 
-info('Done')
+    info('Packing boot image...')
+    pack_cmd = 'tar -zcvf tizen-unified_20181024.1_iot-boot-arm64-rpi3.tar.gz boot.img modules.img' 
+    _exec(pack_cmd.split())
 
-if '-e' in sys.argv:
-    info('Ejecting USB drive')
-    _exec('eject {}'.format(device), shell=True)
+    if args.no_flash:
+        return
+        
+    # flash usb drive
+    info ('Flashing use drive...')
+    device_name = args.device or find_device_name()
+    flash_cmd = './flash-sdcard.sh {}'.format(device_name)
+
+    try:
+        p = sp.Popen('yes', stdout=sp.PIPE)
+        _exec(flash_cmd.split(), stdin=p.stdout)
+    except sp.CalledProcessError as e:
+        error('{} failed with return code {}'.format(flash_cmd, e.returncode))
+
+    info('Done flashing')
+
+    if args.eject:
+        info('Ejecting USB drive')
+        _exec('eject {}'.format(device_name), shell=True)
+
+
+if __name__ == '__main__':
+    build()
+
+
