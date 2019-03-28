f1050ad6534774aa25cda3e515ba009344753b6e
ryanking13 <def6488@gmail.com>
2019-03-29 00:02:12 +0900

    add rotation related struct and syscall prototypes

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,1 +1,49 @@
 /* os proj2 rotation lock header file */
+#ifndef _LINUX_ROTATION_H
+#define _LINUX_ROTATION_H
+
+#include <linux/semaphore.h>
+#include <linux/list.h>
+#include <linux/export.h>
+
+/* ===== rotation_state struct related ===== */
+typedef struct {
+    int degree;
+    int range;
+    // TODO: need an attribute to identify which thread this is?
+    struct list_head next;
+} rotation_lock_list;
+
+typedef struct {
+    int degree;
+    rotation_lock_list read_lock_wait_list;
+    rotation_lock_list write_lock_wait_list;
+    rotation_lock_list read_lock_list;
+    rotation_lock_list write_lock_list;
+    spinlock_t lock; // TODO: change it to read/write lock??
+} rotation_state;
+
+/* global rotation_state struct */
+rotation_state init_rotation;
+EXPORT_SYMBOL(init_rotation);
+
+inline void rotation_lock(rotation_state* rot) {
+    spin_lock(&rot->lock);
+}
+
+inline void rotation_unlock(rotation_state* rot) {
+    spin_unlock(&rot->lock);
+}
+
+inline void rotation_set_degree(rotation_state* rot, int degree) {
+    rot->degree = degree;
+}
+
+inline int is_device_in_rotation(rotation_lock_list* node, rotation_state* rot) {
+    // TODO: check if rotation is inside nodes' LOCK RANGE
+    return 1;
+}
+
+/* ===== rotation_state struct related ===== */
+
+#endif
--- kernel/rotation.c
+++ kernel/rotation.c
@@ -1,10 +1,26 @@
 #include <linux/syscalls.h>
+#include <linux/rotation.h>
+
+// TODO: user memory -> kernel memory copy
 
 /*
  * sets the current device rotation in the kernel.
  * syscall number 398 (you may want to check this number!)
  */
 long set_rotation(int degree) { /* 0 <= degree < 360 */
+    rotation_state *rot = &init_rotation;
+
+    rotation_lock(rot);
+    rotation_set_degree(rot, degree);
+    /* TODO
+        0. check if there some reader/writer is hold a lock for current degree, if so, nothing happens
+        1. check if there are waiting readers
+        2. check if there are waiting writers
+        3. select reader's' or writer to give lock
+        4. awake selected readers/writer
+    */
+    rotation_unlock(rot);
+
     return 0;
 }
 
@@ -14,9 +30,58 @@ long set_rotation(int degree) { /* 0 <= degree < 360 */
  * system call numbers 399 and 400
  */
 long rotlock_read(int degree, int range) {  /* 0 <= degree < 360 , 0 < range < 180 */
+    rotation_state *rot = &init_rotation;
+
+    rotation_lock(rot);
+    // for writer_wait_list
+    //     if there is overlapping lock, fail
+    // for writer_list
+    //     if there is overlapping lock, queue this thread
+    // if not in LOCK_RANGE
+    //     queue this thread
+    // else
+    //     take a reader lock and run this thread
+    rotation_unlock(rot);
+
+    /* TODO
+        1. NO_BLOCK CASE (device must be in LOCK_RANGE)
+            1.1. no other thread is holding a lock.
+            1.2. other threads are holding a lock.
+                1.2.1. other threads holding a lock are readers
+                    1.2.1.1. there is no writer waiting in overlapping range
+                1.2.2. other thread holding a lock is writer
+                    1.2.2.1. there is no writer waiting in overlapping range & current lock is not overlapping
+        2. BLOCK_CASE
+            2.1. add this thread to wait_list and go sleep.
+        3. FAIL_CASE
+            3.1. if writer in overlapping range is waiting, it fails.
+    */
+
     return 0;
 }
 long rotlock_write(int degree, int range) { /* degree - range <= LOCK RANGE <= degree + range */
+    rotation_state *rot = &init_rotation;
+
+    rotation_lock(rot);
+    // for writer_list
+    //     if there is overlapping lock, queue this thread
+    // for reader_list
+    //     if there is overlapping lock, queue this thread
+    // if not in LOCK_RANGE
+    //     queue this thread
+    // else
+    //     take a writer lock and run this thread
+    rotation_unlock(rot);
+    /* TODO
+        1. NO_BLOCK CASE (device must be in LOCK_RANGE)
+            1.1. no other thread is holding a lock.
+            1.2. other threads are holding a lock.
+                1.2.1. current lock is not overlapping
+        2. BLOCK_CASE
+            2.1. add this thread to wait_list and go sleep.
+        3. FAIL_CASE
+            3.1. maybe no fail case?
+    */
     return 0;
 }
 
@@ -26,9 +91,21 @@ long rotlock_write(int degree, int range) { /* degree - range <= LOCK RANGE <= d
  * system call numbers 401 and 402
  */
 long rotunlock_read(int degree, int range) {  /* 0 <= degree < 360 , 0 < range < 180 */
+    rotation_state *rot = &init_rotation;
+
+    rotation_lock(rot);
+    // remove this thread from reader list
+    // then, same as set_rotation
+    rotation_unlock(rot);
     return 0;
 }
 long rotunlock_write(int degree, int range) { /* degree - range <= LOCK RANGE <= degree + range */
+    rotation_state *rot = &init_rotation;
+
+    rotation_lock(rot);
+    // remove this thread from reader list
+    // then, same as set_rotation
+    rotation_unlock(rot);
     return 0;
 }
 
